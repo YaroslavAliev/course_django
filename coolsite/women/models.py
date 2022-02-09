52731ed63b8a430b53b18d2ccaa2f7f6a7773366
YaroslavAliev <yarikaliev121@gmail.com>
2022-02-10 01:56:29 +0200

    15_lesson

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.urls import reverse
 
 class Women(models.Model):
-    title = models.CharField(max_length=250, verbose_name="Заголовок")
-    slug = models.SlugField(max_length=250, unique=True, db_index=True, verbose_name="URL")
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
     content = models.TextField(blank=True, verbose_name="Текст статьи")
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
@@ -31,7 +31,7 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('category', kwargs={'cat_id': self.pk})
+        return reverse('category', kwargs={'cat_slug': self.slug})
 
     class Meta:
         verbose_name = 'Категория'
